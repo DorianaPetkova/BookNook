ec878cd58097b4d0a6b72da736738e266e17420a
Doriana <133523673+DorianaPetkova@users.noreply.github.com>
2025-02-15 10:11:01 +0200

    Quick experiments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,12 @@
+// src/app/layout.tsx (Server Component)
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import ThemeProvider from "@/components/ThemProvider";
 
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+const geistMono = Geist({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "BookNook",
@@ -22,19 +15,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      
-      >
-        <Header/>
-      
-        {children}
-        
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider>
+          <Header />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,27 @@
 import GenreDropDown from "./GenreDropDown";
 import SearchInput from "./SearchInput";
+import ThemeToggler from "./ThemeToggler";
+
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+  } from "@/components/ui/drawer"
 const Header = () => {
     return (
         <div>
            {/*logo and other stuff */} 
           <SearchInput/>
            <GenreDropDown/>
+           <ThemeToggler/>
+           
         </div>
+
         
     );
 };
